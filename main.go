a2cb4672ed30b140e00b4d932935c787cec7b405
michael mccune <msm@opbstudios.com>
2023-11-13 15:10:32 -0500

    update functionality to print total compute resources

--- main.go
+++ main.go
@@ -21,11 +21,19 @@ import (
 	"fmt"
 	"path/filepath"
 
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 
 	machinev1beta1 "github.com/openshift/client-go/machine/clientset/versioned/typed/machine/v1beta1"
+)
+
+const (
+	MASTER_NODE_LABEL = "node-role.kubernetes.io/master"
 )
 
 func main() {
@@ -44,6 +52,47 @@ func main() {
 	}
 
 	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	nodes, err := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("Found %d nodes in the cluster\n", len(nodes.Items))
+	computeNodes := []corev1.Node{}
+	for _, node := range nodes.Items {
+		if _, found := node.Labels[MASTER_NODE_LABEL]; !found {
+			computeNodes = append(computeNodes, node)
+		}
+	}
+	result := corev1.ResourceList{}
+	for _, node := range computeNodes {
+		for resource, value := range node.Status.Capacity {
+			if current, ok := result[resource]; ok {
+				result[resource] = sumQuantity(current, value)
+			} else {
+				result[resource] = value
+			}
+		}
+	}
+	fmt.Printf("%d compute nodes\n", len(computeNodes))
+	fmt.Printf("Compute node resources:\n")
+	for resource, value := range result {
+		fmt.Printf("- %s: %s\n", resource, value.String())
+	}
+}
+
+func sumQuantity(left, right resource.Quantity) resource.Quantity {
+	result := resource.Quantity{}
+	result.Add(left)
+	result.Add(right)
+	return result
+}
+
+func print_machines(config *restclient.Config) {
 	clientset, err := machinev1beta1.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
